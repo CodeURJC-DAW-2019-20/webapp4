03cb79515305a80fd45c171cd03cc66577d11f2d
Miguel Rodriguez Alvarez <miguel7.254@gmail.com>
2020-02-13 09:56:15 +0100

    Lista de apuntes con Controller
    Además configurado para no reinicar server

--- src/main/java/es/urjc/daw/urjc_share/Apunte.java
+++ src/main/java/es/urjc/daw/urjc_share/Apunte.java
@@ -1,40 +1,50 @@
 package es.urjc.daw.urjc_share;
 
 public class Apunte {
-    private String nombre;
-    private String asignatura;
-    private String comentario;
+    private String name;
+    private String subject;
+    private String professor;
+    private String degree;
 
-    public Apunte(){
-
-    }
-    public Apunte(String nombre, String asignatura, String comentario){
-        this.nombre=nombre;
-        this.asignatura=asignatura;
-        this.comentario=comentario;
+    public Apunte() {
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public Apunte(String name, String subject, String professor, String degree) {
+        this.name = name;
+        this.subject = subject;
+        this.professor = professor;
+        this.degree = degree;
     }
 
-    public void setAsignatura(String asignatura) {
-        this.asignatura = asignatura;
+    public String getDegree() {
+        return degree;
     }
 
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
+    public void setDegree(String degree) {
+        this.degree = degree;
     }
 
-    public String getNombre() {
-        return nombre;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getAsignatura() {
-        return asignatura;
+    public void setSubject(String subject) {
+        this.subject = subject;
     }
 
-    public String getComentario() {
-        return comentario;
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getProfessor() {
+        return professor;
     }
 }
--- src/main/java/es/urjc/daw/urjc_share/controllers/Apunte_Controller.java
+++ src/main/java/es/urjc/daw/urjc_share/controllers/Apunte_Controller.java
@@ -5,12 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Controller
 public class Apunte_Controller {
-    @RequestMapping("/subirApunte")
-    public String Apunte_Controller(Model model, Apunte apunte) {
-        model.addAttribute("apunte", apunte);
+
+    private Map<Integer,Apunte> apuntes = new ConcurrentHashMap<>();
+    private AtomicInteger id = new AtomicInteger();
+    @RequestMapping("/apuntes")
+    public String saveFile(Model model, Apunte apunte) {
+        apuntes.put(id.getAndIncrement(), apunte);
+        model.addAttribute("apuntes", apuntes.values());
 
         return "allNotes";
+    }
+    @RequestMapping("/subir_apunte")
+    public String Apunte_Controller() {
+        return "subirApunte";
     }
 }
