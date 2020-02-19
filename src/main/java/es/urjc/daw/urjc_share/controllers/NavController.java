5d5e5d43eb5d017ed27879c07a265cdc59f7a782
davidtb10 <50488430+davidtb10@users.noreply.github.com>
2020-02-19 16:38:56 +0100

    Mapping de de las urls
    Se han añadido redirecciones mediante los mapping tanto para la barra de navegación, como para las búsquedas

--- src/main/java/es/urjc/daw/urjc_share/controllers/NavController.java
+++ src/main/java/es/urjc/daw/urjc_share/controllers/NavController.java
@@ -1,10 +1,14 @@
 package es.urjc.daw.urjc_share.controllers;
 
 import es.urjc.daw.urjc_share.model.Note;
+import es.urjc.daw.urjc_share.model.Subject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -15,23 +19,43 @@ public class NavController {
         return "index";
     }
     
-    @GetMapping("/ranking/")
+    @GetMapping("/ranking")
     public String goToRanking(Model model) {
         return "ranking";
     }
     
-    @GetMapping("/profile/")
+    @GetMapping("/profile")
     public String goToMyProfile(Model model) {
         return "myprofile";
     }
     
-    @GetMapping("/login/")
+    @GetMapping("/login")
     public String goToSignin(Model model) {
         return "login";
     }
     
-    @GetMapping("/join/")
+    @GetMapping("/join")
     public String goToMySignup(Model model) {
         return "register";
     }
+    
+    @PostMapping("/search")
+    public String searchInformation(Model model, @RequestParam String searchType, @RequestParam String textSearched) {
+    	model.addAttribute("textSearched",textSearched);
+    	if(searchType.equals("Grado")) {
+    		return "listsubjects";
+    	}else {
+    		return "allNotes";
+    	}
+    }
+    
+    @PostMapping("/search/subject")
+    public String selectSubject(Model model, @RequestParam String name) {
+    	//TO DO
+    	//Subject must be searched in the database
+    	//and a object must be created and assigned to the model
+    	//model.addAttribute("subject",subject);
+    	return "allNotes";
+    }
+    
 }
