fbd28795c7651da8215c10920023a50013ae2086
Miguel Rodriguez Alvarez <miguel7.254@gmail.com>
2020-02-24 17:11:50 +0100

    Descargar archivo

--- src/main/java/es/urjc/daw/urjc_share/model/Note.java
+++ src/main/java/es/urjc/daw/urjc_share/model/Note.java
@@ -8,7 +8,15 @@ public class Note {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-	
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     private String name;
     private String professor;
     
