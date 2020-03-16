1d7d6ca80bb9af49dd0e5703a7c9cd2709a3daed
David <davidtejerobravo97@gmail.com>
2020-03-16 19:13:26 +0100

    Revert "Merge remote-tracking branch 'origin/Fusion' into Fusion"
    This reverts commit f022a2596a25e2ed738de475eef788368f76b4d2.

--- create_image.sh
+++ create_image.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
 docker network create urjcShare-network 
 
 docker container run --name mysqldb --network urjcShare-network -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=urjc_share -d mysql:8
 
 docker container run --network urjcShare-network --name urjcShare -p 8443:8443 -d davidtb10/urjcshare
-
-docker container stop urjcShare
-docker container stop mysqldb
