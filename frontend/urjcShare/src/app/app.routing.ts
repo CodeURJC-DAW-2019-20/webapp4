c847a993d5f7b5e95684439cb37ef5ac45dc0225
AlexDC <a.domingoc.2017@alumnos.urjc.es>
2020-03-26 14:43:50 +0100

    Página inicio

--- frontend/urjcShare/src/app/app.routing.ts
+++ frontend/urjcShare/src/app/app.routing.ts
@@ -3,12 +3,15 @@ import { ProfileComponent} from './components/profile.component';
 import {Routes, RouterModule} from '@angular/router';
 import {SignInComponent} from "./components/signIn.component";
 import {DegreeListComponent} from "./components/degree-list.component";
+import {IndexComponent} from "./components/index.component";
+
 
 const appRoutes = [
   {path: 'profile', component: ProfileComponent},
   {path: 'signUp', component: SignUpComponent},
   {path: 'signIn', component: SignInComponent},
   {path: 'degrees', component: DegreeListComponent },
+  {path: 'index', component: IndexComponent},
 
 ]
 
---
+++ frontend/urjcShare/src/app/components/index.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  templateUrl: '../html/index.component.html',
+  styleUrls: ['../CSS/index.component.css']
+})
+export class IndexComponent {
+
+
+}
