f906f9be74899e599ce986c90ca6c257c6bc2c88
davidtb10 <50488430+davidtb10@users.noreply.github.com>
2020-03-28 17:20:07 +0100

    Registro de usuarios sin imagen

--- frontend/urjcShare/src/app/components/signUp.component.ts
+++ frontend/urjcShare/src/app/components/signUp.component.ts
@@ -1,13 +1,37 @@
-import { Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../model/user.model";
+import {UserService} from "../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'signUp',
   templateUrl: '../html/signUp.component.html'
 })
-export class SignUpComponent {
-
+export class SignUpComponent implements OnInit{
 
   user: User;
+
+  constructor(private userService: UserService, private router:Router) {}
+
+  ngOnInit(): void {
+    this.user = {
+      name: '',
+      passwordHash: '',
+      degree: ' ',
+      nickname: ' ',
+      email: ' ',
+      number: ' ',
+      roles: ["ROLE_USER"]};
+  }
+
+  save() {
+    this.userService.addUser(this.user).subscribe(
+      book => { },
+      error => console.error('Error creating new user: ' + error)
+    );
+    this.router.navigate(['/']);
+  }
+
+
 
 }
