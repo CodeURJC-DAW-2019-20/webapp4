3c1f2367a5b76eda1ab48db83c9429d2d0071e65
Alvarogv96 <49074454+Alvarogv96@users.noreply.github.com>
2020-04-01 19:14:37 +0200

    Subject Service

---
+++ frontend/urjcShare/src/app/services/subject.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {catchError} from "rxjs/operators";
+import {Observable} from "rxjs";
+import {Subject} from "../model/subject.model";
+
+const BASE_URL = 'api/subjects';
+
+@Injectable({ providedIn: 'root' })
+export class SubjectService{
+
+  constructor(private httpClient: HttpClient) { }
+
+  getSubjectByDegreeId(id: number | string){
+    return this.httpClient.get('api/degrees/' + id + '/subject').pipe(
+      catchError(error => this.handleError(error))
+    ) as Observable<Subject[]>;
+  }
+
+
+
+
+  private handleError(error: any) {
+    console.error(error);
+    return Observable.throw("Server error (" + error.status + "): " + error.text())
+  }
+}
