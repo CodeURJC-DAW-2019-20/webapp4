f0e29b1481b839a1bfcfbee98537521a405138b2
David <davidtejerobravo97@gmail.com>
2020-04-14 17:35:43 +0200

    Frontend dockerizado

---
+++ docker/create_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../frontend/urjcShare
+
+docker run --rm --name front-container -v ${PWD}:/urjcShare -w /urjcShare node:latest /bin/bash -c "npm install; npm run build"
+
+cp -R dist/urjcShare/* ../../backend/src/main/resources/static/new
+
+cd ../..
+
+docker run --rm -v ${PWD}/backend:/backend -w /backend maven:alpine mvn package
+
+docker image build -t davidtb10/urjcshare -f docker/Dockerfile .
